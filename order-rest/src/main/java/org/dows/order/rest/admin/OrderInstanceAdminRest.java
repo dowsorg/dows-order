7ec5937c936498c5fe3848b1fc77e965b9d478b0
liuhonger <Lhe395591190>
2023-01-14 12:17:26 +0800

    fix(订单) 管理后台列表赠送

--- order-rest/src/main/java/org/dows/order/rest/admin/OrderInstanceAdminRest.java
+++ order-rest/src/main/java/org/dows/order/rest/admin/OrderInstanceAdminRest.java
@@ -1,25 +1,19 @@
-package org.dows.order.rest;
+package org.dows.order.rest.admin;
 
 
-import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dows.framework.api.Response;
-import org.dows.order.bo.OrderInstanceQueryBo;
 import org.dows.order.form.OrderInstanceAdminForm;
-import org.dows.order.form.OrderInstanceQueryForm;
 import org.dows.order.service.OrderInstanceService;
 import org.dows.order.vo.OrderInstanceAdminVo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @Api(tags = "订单")
 @Slf4j
@@ -42,4 +36,17 @@ public class OrderInstanceAdminRest {
     public Response<IPage<OrderInstanceAdminVo>> queryOrderInfo(@RequestBody OrderInstanceAdminForm adminForm){
         return Response.ok(null);
     }
+
+
+    /**
+     * 后台订单赠送退菜列表
+     * @param
+     * @return
+     */
+    @PostMapping("/queryGiveOrderInfo")
+    @ApiOperation("后台订单赠送退菜列表")
+    public Response<IPage<OrderInstanceAdminVo>> queryGiveOrderInfo(@RequestBody OrderInstanceAdminForm adminForm){
+        return Response.ok(null);
+    }
+
 }
