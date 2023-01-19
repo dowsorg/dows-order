24e0b8d4c89be3cc1cbd868069ebfa780b3c3114
liuhonger <Lhe395591190>
2023-01-19 16:58:45 +0800

    fix(订单) 查询分页接口

--- order-rest/src/main/java/org/dows/order/rest/tenant/OrderInstanceAdminRest.java
+++ order-rest/src/main/java/org/dows/order/rest/tenant/OrderInstanceAdminRest.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/admin/order")
+@RequestMapping("/order")
 public class OrderInstanceAdminRest {
 
 
