22003c27a513818d39f9133680fd01f5704e744b
liuhonger <Lhe395591190>
2023-02-14 18:16:33 +0800

    自动生成 清理

--- order-pojo/src/main/java/org/dows/order/bo/OrderCommentQueryBo.java
+++ order-pojo/src/main/java/org/dows/order/bo/OrderCommentQueryBo.java
@@ -8,8 +8,11 @@ import java.io.Serializable;
 @Data
 public class OrderCommentQueryBo implements Serializable {
 
-
     private static final long serialVersionUID = 5035401152642897405L;
+
+    @ApiModelProperty(value = "订单评价id 分页用的")
+    private Long commentId;
+
     @ApiModelProperty(value = "门店id")
     private String storeId;
     @ApiModelProperty(value = "类型 0:全部 1:最新 2:好评 3:差评 4:有图有视频 5:回头客评价")
